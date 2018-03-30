7d43f5b5b7758f2ad36f3f4a2a1ddc7e3c9e62c8
mattdvhope <mattagape@me.com>
2018-03-30 15:55:52 -0400

    Set up react

--- webpack/index.js
+++ webpack/index.js
@@ -1,1 +1,13 @@
-console.log('¡Hola Mundo!');
+import React, { Component } from 'react';
+import {render} from 'react-dom';
+import Hello from './components/Hello';
+
+class App extends Component {
+  render() {
+    return (
+	  <Hello />
+	)
+  }
+}
+
+render(<App />, document.getElementById('root'));
