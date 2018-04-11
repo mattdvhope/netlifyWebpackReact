ccab40baef94819af3f15957d22ccadc740eb021
mattdvhope <mattagape@me.com>
2018-04-11 09:53:14 -0400

    Edited fetch

--- webpack/index.js
+++ webpack/index.js
@@ -13,11 +13,16 @@ class App extends Component {
 render(<App />, document.getElementById('root'));
 
 
-// const proxyurl = "https://enigmatic-castle-3874.herokuapp.com/class_times";
-const url = "https://cityenglishproject.com"; // site that doesn’t send Access-Control-*
-fetch(url) // https://enigmatic-castle-3874.herokuapp.com/https://cityenglishproject.com
+// const proxyurl = "https://enigmatic-castle-3874.herokuapp.com/class_times/";
+const url = "https://cityenglishproject.com/class_times"; // site that doesn’t send Access-Control-*
+fetch(url) // https://enigmatic-castle-3874.herokuapp.com/class_times/https://cityenglishproject.com/class_times
 .then(response => response.json())
 .then(res => console.log(res));
+
+
+// fetch(`https://enigmatic-castle-3874.herokuapp.com/class_times`)
+// .then(response => response.json())
+// .then(res => console.log(res));
 
 
 
