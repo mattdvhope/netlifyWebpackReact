4d8b023d1c88e53680a3e969e1cd8f84e62d2edf
mattdvhope <mattagape@me.com>
2018-04-11 10:05:28 -0400

    Edited fetch

--- webpack/index.js
+++ webpack/index.js
@@ -22,6 +22,8 @@ fetch(url, {
       'content-type': 'application/json'
     },
     mode: 'cors', // no-cors, cors, *same-origin
+    redirect: 'follow', // *manual, follow, error
+    referrer: 'no-referrer', // *client, no-referrer
 })
 .then(response => response.json())
 .then(res => console.log(res));
