fdd6615d49233dca94fecb543280855926c7a43a
mattdvhope <mattagape@me.com>
2018-04-25 14:04:41 -0400

    Practiced react-redux

--- build/react_rdx_intermed/actions/index.js
+++ build/react_rdx_intermed/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'c600bcfbb1ee8a4ec5efdd52b4a38e28';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
